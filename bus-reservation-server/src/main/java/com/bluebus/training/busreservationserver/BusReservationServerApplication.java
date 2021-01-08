d30341110fa3779705b70c9e5099ee6f73fa6de0
premmunnaa <premmano98@gmail.com>
2021-01-08 20:16:17 +0530

    First commmit

---
+++ bus-reservation-server/src/main/java/com/bluebus/training/busreservationserver/BusReservationServerApplication.java
@@ -0,0 +1,13 @@
+package com.bluebus.training.busreservationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusReservationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusReservationServerApplication.class, args);
+	}
+
+}
---
+++ bus-reservation-server/src/test/java/com/bluebus/training/busreservationserver/BusReservationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluebus.training.busreservationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusReservationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
